8d62872e01a07fd6e5ca9bd034b60dd9f177e2cd
linhpq0610 <143584755+linhpq0610@users.noreply.github.com>
2023-10-31 12:41:36 +0700

    #1 - linhpq0610 optimized global.php first time

---
+++ core/index.php
@@ -0,0 +1,8 @@
+<?php 
+  require_once "Connection.php";
+  require_once "Database.php";
+  require_once "Model.php";
+  require_once "Controller.php";
+  require_once "Route.php";
+  require_once "Router.php";
+?>
