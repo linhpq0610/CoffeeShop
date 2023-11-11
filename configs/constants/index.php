68c4c452b5993ee52871de5e1e071214e94e4692
linhpq0610 <143584755+linhpq0610@users.noreply.github.com>
2023-11-11 11:56:08 +0700

    11-11-2023, Version 2.1.4
    feat:
      * update sign in feature #19 
    
    PR-URL: #20

--- configs/constants/index.php
+++ configs/constants/index.php
@@ -1,5 +1,5 @@
 <?php 
-  define("COOKIE_LOGIN_NAME", "userId");
+  define("SESSION_LOGIN_NAME", "userId");
   define("WEB_NAME", "Coffee Shop");
   require_once "routes/index.php";
   require_once "directories.php";
