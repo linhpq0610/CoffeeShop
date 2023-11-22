c1440ee2e274e50afd676bfe69f8e1ab607f3a3b
hongmy17 <hongmy172a@gmail.com>
2023-11-22 09:34:35 +0700

    chore: config route xóa mềm category Issue: #62 Task: N6-29

--- configs/routes/admin/category.php
+++ configs/routes/admin/category.php
@@ -1,7 +1,7 @@
 <?php 
   $routes['loai-hang-trang-(\d+)'] = 'admin/category/index/$1';
   $routes['cap-nhat-loai-hang-(\d+)'] = 'admin/category/update/$1';
-  $routes['xoa-loai-hang'] = 'admin/category/delete';
+  $routes['xoa-loai-hang'] = 'admin/category/softDelete';
   $routes['chinh-sua-loai-hang-(\d+)'] = 'admin/category/edit/$1';
   $routes['form-them-loai-hang'] = 'admin/category/showFormAddCategory';
   $routes['them-loai-hang'] = 'admin/category/initAdd';
