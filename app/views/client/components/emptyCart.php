ac37d2f8639113f676611e28b9500e6b3542504e
hongmy17 <hongmy172a@gmail.com>
2023-12-06 22:17:48 +0700

    style: edit cart page

--- app/views/client/components/emptyCart.php
+++ app/views/client/components/emptyCart.php
@@ -1,4 +1,4 @@
 <div class="empty-cart text-center position-absolute top-50 start-50 translate-middle">
-  <img src="<?= IMAGES_URL ?>/empty-cart.jpg" alt="Empty cart">
+  <img style="width: 100%; object-fit: cover" src="<?= IMAGES_URL ?>/empty-cart.png" alt="Empty cart">
   <h5>Giỏ hàng trống</h5>
 </div>
