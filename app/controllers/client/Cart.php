cb1f0f7e4272744608841a208f8a46ef49768ad2
linhpq0610 <143584755+linhpq0610@users.noreply.github.com>
2023-12-08 16:18:36 +0700

    chore: delete picture and Cart.php

--- app/controllers/client/Cart.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  class Cart extends Controller {
-    public function index() {
-      $this->_data['pathToPage'] = CLIENT_VIEW_DIR . '/cart/cart';
-      $this->_data["contentOfPage"] = [];
-      $this->renderClientLayout($this->_data);
-    }
-  }
-?>
