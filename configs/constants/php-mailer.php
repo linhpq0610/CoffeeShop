ad3c05e9a8038c007f0b5b456bf882e6ab4ffe15
linhpq0610 <143584755+linhpq0610@users.noreply.github.com>
2023-12-09 15:13:30 +0700

    feat: update web email
    PR-URL: #92

--- configs/constants/php-mailer.php
+++ configs/constants/php-mailer.php
@@ -1,5 +1,6 @@
 <?php 
+  define("WEB_MAIL", 'coffeeshop596@gmail.com');
   define("SMTP_SERVER", 'smtp.gmail.com');
-  define("SMTP_USERNAME", 'linhpqpc05353@fpt.edu.vn');
-  define("SMTP_PASSWORD", 'plvdbchpexkhkpwh');
+  define("SMTP_USERNAME", WEB_MAIL);
+  define("SMTP_PASSWORD", 'ynifkmfjvpzhxudt');
 ?>
