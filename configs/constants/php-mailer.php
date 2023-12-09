972a2562a462437286663292e359805a8352220b
linhpq0610 <143584755+linhpq0610@users.noreply.github.com>
2023-12-09 23:15:15 +0700

    chore: format config/constant/php-mailer.php

--- configs/constants/php-mailer.php
+++ configs/constants/php-mailer.php
@@ -1,6 +1,6 @@
 <?php 
-    define("WEB_MAIL", 'coffeeshop596@gmail.com');
-    define("SMTP_SERVER", 'smtp.gmail.com');
-    define("SMTP_USERNAME", 'tranchinguyen1307@gmail.com');
-    define("SMTP_PASSWORD", 'qdog dsph xbkw eirq');
+  define("WEB_MAIL", 'coffeeshop596@gmail.com');
+  define("SMTP_SERVER", 'smtp.gmail.com');
+  define("SMTP_USERNAME", 'tranchinguyen1307@gmail.com');
+  define("SMTP_PASSWORD", 'qdog dsph xbkw eirq');
 ?>
